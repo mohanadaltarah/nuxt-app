8f500d8ce1570fd637af761e7b7f5d44be11a938
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-11-08 11:01:30 +0300

    Build a 404 status code page

---
+++ error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="error d-flex align-center justify-center" style="height: 100vh">
+    <v-card class="ma-auto w-50 text-center pa-8">
+      <v-card-title>
+        Error: {{ error.statusCode }}
+      </v-card-title>
+      <v-card-title>
+        {{ error.statusMessage }}
+        {{ error }}
+      </v-card-title>
+      <v-card-text>
+        {{ error }}
+      </v-card-text>
+      <v-btn @click="$router.push({name: 'index'})">
+        back to homepage
+      </v-btn>
+    </v-card>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+defineProps(["error"])
+</script>
+
+
+<style scoped>
+
+
+</style>
